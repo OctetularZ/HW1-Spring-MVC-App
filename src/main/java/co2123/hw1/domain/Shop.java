d86bca34c9ff4bbdc29553755ce95b7d329f1c5b
OctetularZ <taiabayomi15@gmail.com>
2024-11-11 20:34:56 +0000

    Initial commit

---
+++ src/main/java/co2123/hw1/domain/Shop.java
@@ -0,0 +1,27 @@
+package co2123.hw1.domain;
+
+public class Shop {
+    private String boss;
+
+    private String headquarters;
+
+    private String industry;
+
+    private int founded;
+
+    public String getBoss() {return boss;}
+
+    public void setBoss(String boss) {}
+
+    public String getHeadquarters() {return headquarters;}
+
+    public void setHeadquarters(String headquarters) {this.headquarters = headquarters;}
+
+    public String getIndustry() {return industry;}
+
+    public void setIndustry(String industry) {this.industry = industry;}
+
+    public int getFounded() {return founded;}
+
+    public void setFounded(int founded) {this.founded = founded;}
+}
---
+++ src/main/webapp/WEB-INF/views/start.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<h2>Mall Management System</h2>
+<p>
+	<a href="/malls">List malls</a>
+</p>
+</body>
+</html>
